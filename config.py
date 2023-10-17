86a2461830a46e2d5f6fb45ea3b70cdb276e9e65
aarnold1620 <alexarnold2024@u.northwestern.edu>
2023-10-17 14:46:58 -0500

    Added to config file, script for getting hosts

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
 class config:
     max_presenters = 2
     num_noms = 5         
-    num_hosts = 2                                                                                                                                                                                                                                                   
+    num_hosts = 2
+    datapath = os.path.join(os.curdir, "data/gg2013.json")                                                                                                                                                                                                                                                   
---
+++ frames.py
@@ -0,0 +1,68 @@
+from config import config
+import json
+import os
+import spacy
+from ftfy import fix_text
+import re 
+from collections import Counter
+
+
+def is_actor(name):
+    #Implement with MongoDB
+    return True
+
+def is_movie(name):
+    #Implement with MongoDB
+    return True
+
+def could_win(winner,award):
+    return winner in award.nominees 
+
+class award():
+    def __init__(self, award) -> None:
+        self.award = award
+        self.nominees = []
+        self.winner = None
+    def addNominee(self, name):
+        if(len(self.nominees) > config.num_noms):
+            return
+        elif(not is_actor(name)):
+            return
+        else:
+            self.nominees.append(name)
+    def set_winner(self, winner):
+        if could_win(winner):
+            self.winner = winner
+           
+
+spacy_model = spacy.load("en_core_web_sm")   
+tweet_json_path = config.datapath
+
+f = open(tweet_json_path)
+data = json.load(f)
+text = [d['text'] for d in data]
+host_words = [h for h in text if re.search("host(?:s|ing|ed)?",h, re.IGNORECASE)]
+names = []
+for t in host_words:
+    output = spacy_model(t)
+    
+    for entity in output.ents:
+        
+        if entity.label_ == "PERSON":
+            
+            names.append(entity.text)
+counter = Counter(names)
+top = counter.most_common(20)
+two_name = [s for s in top if len(s[0].split()) ==2] # add actor check
+one_name = [s for s in top if len(s[0].split()) ==1]
+final_list = []
+for name2, count in two_name:
+    for name1, count1 in one_name:
+        if name1 in name2:
+            count+=count1
+    final_list.append((name2,count))
+print(final_list)
+final_list = sorted(final_list, key = lambda x: -x[1])
+print(final_list[:2])
+
+    
