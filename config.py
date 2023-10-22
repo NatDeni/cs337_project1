b285bef8aca40d3ea90ebdcce5c9e7ff53cbd5f6
Natalia <nataliadenisenko2027@u.northwestern.edu>
2023-10-22 16:45:13 -0500

    Update config variable for awatrd categories

--- config.py
+++ config.py
@@ -2,11 +2,14 @@ import os
 from pymongo import MongoClient
 conn = MongoClient('localhost', 27017)
 db = conn.awards
+
 class config:
     max_presenters = 2
     num_noms = 5         
     num_hosts = 2
     datapath = os.path.join(os.curdir, "data/gg2013.json")
-    preproc_datapath = os.path.join(os.curdir, "data/gg2013_preprocessed.json")
+    preproc_datapath = os.path.join(os.curdir, "data/gg2013_preprocessed_01.json")
+    award_datapath = os.path.join(os.curdir, "data/categories.json")
     actors = db.actors
-    movies = db.movies                                                                                                                                                                                                                                                   
+    movies = db.movies
+    
