12a3a495eff1c4be2de3c9766ecbc40721533c0e
Natalia <nataliadenisenko2027@u.northwestern.edu>
2023-10-24 17:57:02 -0500

    Updated config

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class config:
     max_presenters = 2
     num_noms = 5         
     num_hosts = 2
+    answers = os.path.join(os.curdir, "data/gg2013answers.json")
     datapath = os.path.join(os.curdir, "data/gg2013.json")
     preproc_datapath = os.path.join(os.curdir, "data/gg2013_preprocessed_03.json")
+    preproc_special_datapath = os.path.join(os.curdir, "data/gg2013_preprocessed_special.json")
     award_datapath = os.path.join(os.curdir, "data/categories.json")
+    json_output = os.path.join(os.curdir, "data/our_output_json.json")
+    readable_output = os.path.join(os.curdir, "data/our_output_readable.txt")
     actors = db.actors
     movies = db.movies
-
-    
